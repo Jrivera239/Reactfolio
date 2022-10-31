4f384a2f40ff6f16c780fe395de8f0cd5d300c01
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-10-31 11:02:02 -0400

    npm install

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React, { useState } from 'react';
-import Header from './components/Header';
+
 import About from './components/About';
-import Project from './components/Project';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
+
 
 export default function App() {
   
   const [currentPage, setCurrentPage] = useState('About')
   
   const selectedPage = () => {
-    if (currentPage === 'About') {
-      return <About />
-    }
-    if (currentPage === 'Contact') {
-      return <Contact />
-    }
-    if (currentPage === 'Portfolio') {
-      return <Project />
-    }
-  }
-  
+
   const pageChange = (page) => setCurrentPage(page);
     return (
     <>
